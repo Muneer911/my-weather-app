5e2861a8403d6c0087ac36db67181df3bca2d183
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-20 23:07:50 +0200

    fix: typo in import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import NavBar from "./Components/NavBar/navBar";
-import Body from "./Components/Body/body";
+import Body from "./Components/Body/Body";
 import "./App.css";
 function App() {
   return (
