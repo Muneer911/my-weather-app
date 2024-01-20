93d1dad7229090b1e0d755751417471acae956b4
Muneer <ahmed.elsadig.ahmed@gmail.com>
2024-01-20 21:52:25 +0200

    Add: three weather icons to the assets

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ import Cloud from "./assets/Cloud.png";
 import Search from "./assets/Search.png";
 import Search2 from "./assets/Search2.png";
 import Sunny from "./assets/Sunny.png";
+import Thunderstorm from "./assets/Thunderstorm.png";
+import Clear from "./assets/Clear.png";
+import Snow from "./assets/Snow.png";
 
-export { Rain, Cloud, Search, Search2, Sunny };
+export { Rain, Cloud, Sunny, Clear, Snow, Thunderstorm, Search, Search2 };
